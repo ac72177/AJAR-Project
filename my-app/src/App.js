546d45ef377f2a971409b2a7b6f0b484bd1de534
renehuang1999 <renehuang1999@gmail.com>
2022-06-09 18:57:27 -0700

    added v1 garden view using grid. still need to figure out how to turn background transparent, remove "item", and put different images base on conditions.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Login } from "./components/login/login";
 import { Signup } from "./components/login/signup";
+import Garden from "./components/garden";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
+                <Route path='/home' element={<Garden />} />
             </Routes>
         </BrowserRouter>
     </div>

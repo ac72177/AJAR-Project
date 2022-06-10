43931ec14b97a000add0e3e078102b5794ab1bf6
jun <jun585jun@gmail.com>
2022-06-09 19:19:37 -0700

    main skeleton structure of components before css

---
+++ my-app/src/components/SubPlan.js
@@ -0,0 +1,10 @@
+export default function SubPlan(props) {
+    let subPlan = props.subPlan; 
+    return (
+        <div>
+            <h3>{subPlan.name}</h3>
+            <h4> {subPlan.description} </h4>
+            <h4> </h4>
+        </div>
+    )
+}

6bea95dbf8cc1e6691b47b6c67a1cb0d89328371
jun <jun585jun@gmail.com>
2022-06-14 00:38:59 -0700

    added endpoint to retrieve a single user's data (dummy data)

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const dummyData = require('../dummyData.json')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET user. */
+// Endpoint for retrieving a single user.
+// *Set for dummy data for now.
+router.get('/:user', function(req, res, next) {
+    console.log('got here');
+    res.send(dummyData);
 });
 
 module.exports = router;

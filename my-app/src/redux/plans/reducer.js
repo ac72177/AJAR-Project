e1f489c070feff7dda5ca11a861ac9017062ae04
andreasy <ac72177@yahoo.com>
2022-06-21 21:49:15 -0700

    small modifications, might revert this commit

--- my-app/src/redux/plans/reducer.js
+++ my-app/src/redux/plans/reducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { REQUEST_STATE } from '../utils';
-import { addPlanAsync, getPlansAsync, deletePlanAsync, putPlanAsync } from './thunks';
+import { addPlanAsync, getPlansAsync, deletePlanAsync } from './thunks';
 
 const INITIAL_STATE = {
     list: [],

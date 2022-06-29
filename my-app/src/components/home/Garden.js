f72fe2db5a8b3fbb4a90aca253a755a261d7e01c
renehuang1999 <renehuang1999@gmail.com>
2022-06-28 18:25:17 -0700

    garden wip

--- my-app/src/components/home/Garden.js
+++ my-app/src/components/home/Garden.js
@@ -1,80 +1,76 @@
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
+import plant1 from "../../img/plant1.png"
+import plant2 from "../../img/plant2.png"
 import plant3 from "../../img/plant3.png"
 
 
 const useStyles = makeStyles((theme) => ({
-        paper: {
-            backgroundColor: "transparent",
-            padding: theme.spacing(4),
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundImage: `url(${plant3})`,
-        }
+    paper1: {
+        backgroundColor: "transparent",
+        padding: theme.spacing(5),
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${plant1})`,
+    },
+    paper2: {
+        backgroundColor: "transparent",
+        padding: theme.spacing(5),
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${plant2})`,
+    },
+    paper3: {
+        backgroundColor: "transparent",
+        padding: theme.spacing(5),
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${plant3})`,
+    },
+
 }))
 
-function GridItem({classes}) {
+function Plant1({classes}) {
     return (
         <Grid plant xs={20}>
-            <Paper className={classes.paper}> </Paper>
+            <Paper className={classes.paper1}> </Paper>
         </Grid>
     )
 }
 
-export default function Garden() {
+function Plant2({classes}) {
+    return (
+        <Grid plant xs={20}>
+            <Paper className={classes.paper2}> </Paper>
+        </Grid>
+    )
+}
+
+function Plant3({classes}) {
+    return (
+        <Grid plant xs={20}>
+            <Paper className={classes.paper3}> </Paper>
+        </Grid>
+    )
+}
+
+export default function Garden(props) {
+    const plans = props.plans;
+
     const classes = useStyles();
 
     return (
         <div className={"garden"}>
             <div className={"garden_background"}>
                 <Grid container spacing={2.5} >
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
-                    <GridItem classes = {classes} />
+                    <Plant1 classes = {classes} />
+                    <Plant2 classes = {classes} />
+                    <Plant3 classes = {classes} />
+                    <Plant3 classes = {classes} />
                 </Grid>
             </div>
         </div>

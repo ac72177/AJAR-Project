70d68a90851420a4068f5ad93953239abea05bbd
jun <jun585jun@gmail.com>
2022-07-14 18:49:25 -0700

    fixed del and put cases in reducer.js

--- my-app/src/redux/plans/reducer.js
+++ my-app/src/redux/plans/reducer.js
@@ -35,7 +35,7 @@ const INITIAL_STATE = {
         })
         .addCase(deletePlanAsync.fulfilled, (state, action) => {
             state.deletePlan = REQUEST_STATE.FULFILLED;
-            state.list = action.payload;
+            state.list = [];
         })
         .addCase(deletePlanAsync.rejected, (state, action) => {
             state.deletePlan = REQUEST_STATE.REJECTED;
@@ -60,7 +60,8 @@ const INITIAL_STATE = {
         })
         .addCase(putPlanAsync.fulfilled, (state, action) => {
             state.putPlan = REQUEST_STATE.FULFILLED;
-            state.list = action.payload;
+            let index = state.list.findIndex(plan => plan._id === action.payload._id)
+            state.list[index] = action.payload;
         })
         .addCase(putPlanAsync.rejected, (state, action) => {
             state.putPlan = REQUEST_STATE.REJECTED;

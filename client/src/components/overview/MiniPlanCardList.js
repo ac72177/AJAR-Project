76a65873bbfd44d7c64756fcca6158e29d65b62c
renehuang1999 <renehuang1999@gmail.com>
2022-08-05 15:01:53 -0700

    addede MiniPlanCardList.js

---
+++ client/src/components/overview/MiniPlanCardList.js
@@ -0,0 +1,17 @@
+import MiniPlanCard from "./MiniPlanCard";
+
+function MiniPlanCardList(props){
+    console.log(props);
+    console.log("props.plans =" + props.plans);
+    // const plans = props.plans;
+    //
+    // const listComponents = plans.map((object, index) => {
+    //     return <MiniPlanCard key={index} id={object} />;
+    // });
+
+    return (
+        <p> hi </p>
+        // <ul className="section grid-container">{listComponents}</ul>
+    )
+}
+export default MiniPlanCardList;
--- client/src/pages/GardenView.js
+++ client/src/pages/GardenView.js
@@ -8,6 +8,7 @@ export default function GardenView() {
   return (
     <div className={"home"}>
 
+
       <SummaryBarContainer />
 
       <GardenContainer />

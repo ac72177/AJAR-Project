d68a4c5dab406b8af946927bfef76c88f9edb296
Andrew Chang <andrew.chang@thrive.health>
2022-08-07 04:37:29 -0700

    change welcome msg depending on time of day

--- client/src/components/home/WelcomeMsg.js
+++ client/src/components/home/WelcomeMsg.js
@@ -5,8 +5,19 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export default function WelcomeMsg() {
   const { user } = useAuth0();
-
   const dispatch = useDispatch();
+  let greeting = "";
+  const date = new Date();
+  const hour = date.getHours();
+  if (hour >= 0 && hour < 5) {
+    greeting = "Happy owl hours";
+  } else if (hour >= 5 && hour < 12) {
+    greeting = "Good morning";
+  } else if (hour >= 12 && hour < 18) {
+    greeting = "Good afternoon";
+  } else if (hour >= 18 && hour < 24) {
+    greeting = "Good evening";
+  }
 
   useEffect(() => {
     dispatch(getUserAsync(user.sub));
@@ -19,7 +30,10 @@ export default function WelcomeMsg() {
       <div className="Welcome-message">
         <h1>
           {" "}
-          <strong> Hi {returnedUser.name_first}! </strong>{" "}
+          <strong>
+            {" "}
+            {greeting}, {returnedUser.name_first}{" "}
+          </strong>{" "}
         </h1>
       </div>
     );

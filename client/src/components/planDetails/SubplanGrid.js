c9291d30555f65a824df39070b66f52b90a8af14
jun <jun585jun@gmail.com>
2022-08-09 01:59:09 -0700

    Updated grid size

--- client/src/components/planDetails/SubplanGrid.js
+++ client/src/components/planDetails/SubplanGrid.js
@@ -6,47 +6,52 @@ import { DataGrid } from '@mui/x-data-grid';
 import { red } from '@mui/material/colors';
 import { useDispatch } from 'react-redux';
 import { putPlanAsync } from '../../redux/plans/thunks';
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 import { stringToDate } from "../../utils/dateFormat";
 
 export default function SubPlanGrid(props) {
     const rows = props.plan.plans
+    const { user } = useAuth0();
     const dispatch = useDispatch()
 
+    const checkBox = (plan, subplan) => {
+        let tasks = plan.plans;
+        let data = [...tasks];
+        let task = {
+            _id: subplan._id,
+            name: subplan.name,
+            isComplete: !subplan.isComplete,
+        };
+
+        let index = tasks.findIndex((task) => task._id === subplan._id);
+
+        data[index] = task;
+
+        let checkComplete;
+        for (let i = 0; i < plan.plans.length; i++) {
+            if (data[i].isComplete == true) {
+                checkComplete = true;
+            } else {
+                checkComplete = false;
+                break;
+            }
+        }
+
+        console.log("Complete:" + checkComplete);
+
+        let request = {
+            _id: plan._id,
+            plans: data,
+            isComplete: checkComplete,
+            owner: user.sub,
+        };
+
+        dispatch(putPlanAsync(request));
+    };
+
     const columns = [
-        { field: 'name', headerName: 'Name', width: 150 },
-        {
-            field: 'startDate',
-            headerName: 'Start Date',
-            width: 90,
-            renderCell: (params) => {
-                console.log(params)
-                if (params.value) {
-                    const date = stringToDate(params.value)
-                    return (
-                        <div>{date}
-                        </div>)
-                } else {
-                    <div></div>
-                }
-            }
-        },
-        {
-            field: 'dueDate',
-            headerName: 'Due Date',
-            width: 90,
-            renderCell: (params) => {
-                if (params.value) {
-                    const date = stringToDate(params.value)
-                    return (
-                        <div>{date}
-                        </div>)
-                } else {
-                    <div></div>
-                }
-            }
-        },
+        { field: 'name', headerName: 'Name', width: 200 },
         {
             field: 'isComplete',
             headerName: 'Done?',
@@ -55,7 +60,7 @@ export default function SubPlanGrid(props) {
                 if (params.value) {
                     return (
                         <div>
-                            <IconButton aria-label="Check">
+                            <IconButton aria-label="Check" onClick={() => checkBox(props.plan, params.row)}>
                                 <CheckIcon color="success" />
                             </IconButton>
                         </div>
@@ -63,18 +68,19 @@ export default function SubPlanGrid(props) {
                 } else {
                     return (
                         <div>
-                            <IconButton aria-label="Clear">
-                                <ClearIcon sx={{ color: red[300] }}/>
+                            <IconButton aria-label="Clear" onClick={() => checkBox(props.plan, params.row)}>
+                                <ClearIcon sx={{ color: red[300] }} />
                             </IconButton>
                         </div>
                     )
                 }
             }
-    
+
         }
     ];
+
     return (
-        <Box sx={{ height: 400, width: '80%' }}>
+        <Box sx={{ height: 400, width: '40%' }}>
             <DataGrid
                 rows={rows}
                 columns={columns}
@@ -86,38 +92,3 @@ export default function SubPlanGrid(props) {
         </Box>
     );
 }
-
-// const checkBox = (plan, subplan) => {
-//     let tasks = plan.plans;
-//     let data = [...tasks];
-//     let task = {
-//       _id: subplan._id,
-//       name: subplan.name,
-//       isComplete: !subplan.isComplete,
-//     };
-
-//     let index = tasks.findIndex((task) => task._id === subplan._id);
-
-//     data[index] = task;
-
-//     let checkComplete;
-//     for (let i = 0; i < plan.plans.length; i++) {
-//       if (data[i].isComplete == true) {
-//         checkComplete = true;
-//       } else {
-//         checkComplete = false;
-//         break;
-//       }
-//     }
-
-//     console.log("Complete:" + checkComplete);
-
-//     let request = {
-//       _id: plan._id,
-//       plans: data,
-//       isComplete: checkComplete,
-//       owner: user.sub,
-//     };
-
-//     dispatch(putPlanAsync(request));
-//   };
